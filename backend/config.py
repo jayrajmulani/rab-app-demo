707ca3225f9a8a87124a2b582fd2bfc822aa7245
Jayraj Mulani <jayrajm.mulani@gmail.com>
2022-08-30 00:06:11 -0400

    Add Docker files + Move Db, Data

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,6 @@ import os
 
 # Database configuration
 
-db_file = "../db/rab_data.db"
-movements = "../data/movement.csv"
-population = "../data/population.csv"
+db_file = "./db/rab_data.db"
+movements = "./data/movement.csv"
+population = "./data/population.csv"
